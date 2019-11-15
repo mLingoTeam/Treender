49450677be3c6061ddf0aa27de864b7dbb9ee16f
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2019-11-15 19:23:33 +0100

    Fundamental layout

---
+++ Treender/ClientApp/src/components/Logo.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./NavMenu.css";
+
+class Logo extends Component {
+  render() {
+    return (
+      <span class="navbar navbar-default">
+        <span class="container-fluid">
+          <span class="navbar-header">
+            <a class="navbar-brand" href="#">
+              Treender
+            </a>
+          </span>
+        </span>
+      </span>
+    );
+  }
+}
+
+export default Logo;
