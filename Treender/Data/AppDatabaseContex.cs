65f4ddfa50b068054c5b8cb91b7ae0b52fddcc00
Michal <mszostakk01@gmail.com>
2019-11-15 18:50:45 +0100

    Add and configure dbcontext service

---
+++ Treender/Data/AppDatabaseContex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Treender.Data
+{
+
+    /// <summary>
+    /// Class holding database context of the application
+    /// </summary>
+    public class AppDatabaseContex : DbContext
+    {
+
+        /// <summary>
+        /// Default constructor that configures database context
+        /// </summary>
+        /// <param name="options">Options passed to db context</param>
+        public AppDatabaseContex(DbContextOptions<AppDatabaseContex> options)
+        {
+            
+        }
+    }
+}
