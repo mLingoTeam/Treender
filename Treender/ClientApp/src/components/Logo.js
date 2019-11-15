5dce7be3479d76db2c34d2d98780a9e9046e74b0
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2019-11-15 19:44:39 +0100

    Add some basic components

--- Treender/ClientApp/src/components/Logo.js
+++ Treender/ClientApp/src/components/Logo.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
+import Signup from "./Singup";
 import "./NavMenu.css";
 
 class Logo extends Component {
   render() {
     return (
-      <span class="navbar navbar-default">
-        <span class="container-fluid">
-          <span class="navbar-header">
-            <a class="navbar-brand" href="#">
-              Treender
-            </a>
+      <div>
+        <span class="navbar navbar-default">
+          <span class="container-fluid">
+            <span class="navbar-header">
+              <a class="navbar-brand" href="#">
+                Treender
+              </a>
+            </span>
           </span>
         </span>
-      </span>
+        <Signup />
+      </div>
     );
   }
 }
