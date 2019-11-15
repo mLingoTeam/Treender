f7dae93276541c3afdb0403627ed8d4e7b407aea
Michal <mszostakk01@gmail.com>
2019-11-15 23:08:29 +0100

    Add user database object model

---
+++ Treender/Data/DbModels/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Treender.Data.DbModels
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+        public Guid Uid { get; set; }
+
+        public int Preferences { get; set; }
+    }
+}
