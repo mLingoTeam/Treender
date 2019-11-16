6ee6b68f5d671f07e80c6ca2178240062181e776
Michal <mszostakk01@gmail.com>
2019-11-16 01:55:34 +0100

    database operations to add new user with default prefernces

--- Treender/Controllers/HomeController.cs
+++ Treender/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Logging;
 using Treender.Data;
 using Treender.Data.DbModels;
@@ -25,6 +23,31 @@ namespace Treender.Controllers
         [Route("/Login")]
         public async Task LoginAsync()
         {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = await reader.ReadToEndAsync();
+
+                //create default user prefernces
+                Preference pref = new Preference
+                {
+                    Uid = new Guid(),
+                    MaxHeight = 0,
+                    MinHeight = 0,
+                    Specie = 1,
+                    Type = 1
+                };
+
+                _dbContext.Preferences.Add(pref);
+                
+                //create new user with default preferences
+                _dbContext.Users.Add(new User
+                {
+                    Username = body,
+                    Uid = new Guid(),
+                    Preferences = pref.Uid
+                });
+                _dbContext.SaveChanges();
+            }
         }
     }
 }
